69aa7b67a639225c41d2d7e1508382d20fdfd0d8
Andrey Adamovich <gooddha@ya.ru>
2021-10-10 22:08:45 +0300

    change from master

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -7,14 +7,20 @@ const Modal = (props) => {
   const [inputValue, setInputValue] = useState('');
 
   const handleChange = useCallback((e) => {
-    let newValue = e.target.value;
-
-    setInputValue(newValue);
+    setInputValue(e.target.value);
   }, []);
+
+  const handleRunBuild = useCallback((e) => {
+    if (inputValue.length > 0) {
+      setModal(false);
+    } else {
+      alert('Enter commit hash value');
+    }
+  }, [inputValue]);
 
   const handleCancel = useCallback(() => {
     setModal(false);
-  }, [])
+  }, []);
 
 
   return (
@@ -34,7 +40,7 @@ const Modal = (props) => {
           <button className="grey-button clear-input"></button>
         </label>
         <div className="buttons">
-          <button type="submit" className="yellow-button" onClick={handleCancel}>Run build</button>
+          <button type="submit" className="yellow-button" onClick={handleRunBuild}>Run build</button>
           <Link to="/"><button className="grey-button" onClick={handleCancel}>Cancel</button></Link>
         </div>
       </div>
